b1a514ebe048ab4faf889d59052749c39f523d6d
khoa <agnus1904@gmail.com>
2020-08-15 13:34:56 +0700

    Add feedback form and check price form

--- script/script.js
+++ script/script.js
@@ -3,6 +3,8 @@ const navOutTop = document.querySelector(".nav-out-top");
 
 const btn = document.querySelector(".navbar-toggler");
 
+const btnBooking = document.getElementById("checkprice");
+
         window.addEventListener("scroll", () => {
             if (window.pageYOffset > 100){
                 toTop.classList.add("active");
@@ -18,3 +20,43 @@ const btn = document.querySelector(".navbar-toggler");
                     navOutTop.classList.add("nav-out-top");
             }
         });
+
+        btnBooking.addEventListener("click", function(){
+            var adultNum = document.getElementById("adultNum").value;
+            var childNum = document.getElementById("childNum").value;
+            var hotelInput = document.getElementById("cars").value;
+            var hotelPrice;
+            if(hotelInput === "hotel1"){
+                hotelPrice = 30; 
+            }
+            if(hotelInput === "hotel2"){
+                hotelPrice = 25; 
+            }
+            if(hotelInput === "hotel3"){
+                hotelPrice = 22; 
+            }
+
+            var pri1,pri2,pri3,pri4;
+
+            pri1 = (parseInt(adultNum)*5) + (parseInt(childNum)*3) + hotelPrice;
+            pri2 = (parseInt(adultNum)*7) + (parseInt(childNum)*4) + hotelPrice;
+            pri3 = (parseInt(adultNum)*10) + (parseInt(childNum)*6) + hotelPrice;
+            pri4 = (parseInt(adultNum)*15) + (parseInt(childNum)*9) + hotelPrice;
+            
+            document.getElementById("default").innerText = "The price is: " + pri1 + "$";
+            document.getElementById("silver").innerText = "The price is: " + pri2 + "$";
+            document.getElementById("gold").innerText = "The price is: " + pri3 + "$";
+            document.getElementById("fullcombo").innerText = "The price is: " + pri4 + "$";
+            document.getElementById("form-booking").classList.add("show");
+        });
+
+        function booking(){
+            alert("We have received your information. please wait for our contact");
+            location.reload();
+        }
+
+        function feedBack(){
+            alert("We have get your feedback, Thanks for visiting my website");
+            document.getElementById("email-feedback").value = "";
+            document.getElementById("text-feedback").value = "";
+        }
